de732d5e810d1c458febe2db367aca84ed16b41c
brianpompey <brianthescholar@gmail.com>
2021-09-01 21:34:41 -0400

    working on labels and entry

--- loan_calculator.py
+++ loan_calculator.py
@@ -15,3 +15,19 @@ def __init__(self):
     Label(window, text = "Monthly Payment").grid(row=4, column=1, sticky= W)
 
     Label(window, text = "Total Payment").grid(row=5, column=1, sticky= W)
+
+
+    self.annualInterestRateVar = StringVar()
+    Entry(window, textvariable= self.annualInterestRateVar, justify= RIGHT).grid(row=1, column=2)
+
+    self.numberOfYearsVar = StringVar()
+    Entry(window, textvariable= self.numberOfYearsVar, justify= RIGHT).grid(row=2, column=2)
+
+    self.loanAmmountVar = StringVar()
+    Entry(window, textvariable= self.loanAmmountVar, justify= RIGHT).grid(row=3, column=2)
+
+    self.monthlyPaymentVar = StringVar()
+    
+
+    self.totalPaymentVar = StringVar()
+    Entry(window, textvariable= self.annualInterestRateVar, justify= RIGHT).grid(row=1, column=2)
